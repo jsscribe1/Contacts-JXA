0784ac14ca82ea477a54665bfe710ddb5ff2e988
jsscribe <81885417+jsscribe1@users.noreply.github.com>
2024-06-06 22:19:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Contacts-JXA
+This is a simple automation script that runs in the MacOS script editor.  It will read your Apple Contacts and create a text file on the desktop that lists these contacts in alphabetical order based on a contact's first name.
+
+# Setting Up
+The MacOS Script Editor is used here to run this script.  The Script Editor app is located in the /Aplications/Utilities folder and comes built-in on MacOS. To get this code to run on your mac, you will first need to open the Script Editor app on your mac and then copy this code into a new blank script window that you create. Unfortunately, the Script Editor can't open plain javascript or text files. So you can't just download the contacts-jxa.js file here and try to open it in the editor directly. You will have to create a new script file in the editor and paste the code here into that editor window.  Annoying, but it's the easiest way to do this.
+
+For this situation, open the Script Editor first and select File>New to create a new script.  Copy the raw text from the contacts-jxa.js file and paste it into that script editor window manually. Now you can save and run this code in your script editor. Make sure that you select "JavaScript" and not "AppleScript" for the script type, as shown below (AppleScript is the default).
+
+![image](https://github.com/jsscribe1/Contacts-JXA/assets/81885417/c7a36edb-2c7d-4500-a977-3261240c1658)
+
+# Running the App
+After pasting the contacts-jxa.js code into the Script Editor window for the first time, read through the code and make sure you understand what it does.  The script simply reads your apple contacts and lists them on a text file. To run the script, click on the Run icon (triangle) at the top right corner of the Script Editor.
+
+When running the script for the first time, you will be prompted to let the script have access to your apple contacts. You will see two prompts (shown below). Click OK both times to allow the script to access your contacts. Depending on how many contacts you have, it may take 30 seconds or more for the script to read your contact list and write the list to the text file which will appear on your desktop. When finished, the result pane below the editor should say "script has been run" and a file should appear on the desktop called contacts-list.txt.  If you make any changes to the script, click on the Complile icon (hammer) near the Run icon before attempting to run the script again (this will prevent a compile error).
+
+![script-warn1](https://github.com/jsscribe1/Contacts-JXA/assets/81885417/1033a218-3453-4a35-b0f3-dfa42c239493)
+![script-warn2](https://github.com/jsscribe1/Contacts-JXA/assets/81885417/98ba55cb-348a-4c21-a1b9-6ed7a6b9d3a2)
+![script-warn3](https://github.com/jsscribe1/Contacts-JXA/assets/81885417/af49b54a-cb44-49c0-99b9-114598f2fbf5)
+
+
+# Things to keep in mind
+The Apple Script Editor has a few quirks.  First, the Script Editor only fully compiles code after you make some change to a script or if you click the compile button before clicking the run button.  If you click the run button for the same script a second time without making any changes, you will see an error that tells you that you can't duplicate your variable names. If you don't make changes you have to compile (e.g., click compile) the script each time before running it.
+
+Second, even when the script runs properly, it is extremely slow for some reason. As mentioned before, the above script may take more than a minute to run when there are a lot of contacts, or if your mac is older. Considering that all the resources for the script are local (not over a network), I can't say why this is.
+
+Third, if the Apple Script Editor encounters an error while running a script, it may not alert you properly or exit gracefully. You may have to force-quit the app, and in some cases reboot your mac in order for the Script Editor app to work properly again. You can of course manually try to kill the processes and close the open-file-handles that the app is using, but mostly it's just easier to reboot (as bad as that sounds). 
+
+Forth, The Apple Script Editor is not a good tool for doing complex JavaScript development.  There is no built-in debugging, and though you can connect your script to Safari's Web Inspector, it shows you only basic debug info and you have to be careful about stepping into certain native JavaScript functions (the debugger may hang up).
+
+Lastly, the JavaScript for OSX Automation platform is not well supported or even well documented by Apple. There also isn't a lot of good documentation by people outside of Apple on the web.  Consequently, finding code examples is difficult, and creating your own new code requires a lot of experimentation.
+
+All in all, the JXA platform and the script editor quirks make serious development tedious.  But for simple scripts and simple tasks, it's good enough. 
+
+
---
+++ contacts-jxa.js
@@ -0,0 +1,182 @@
+ * List-Contacts-JXA
+ *
+ * Simple script that reads the information in Apple Contacts and
+ * creates a text file list on the desktop.
+ * The contacts are sorted aphabetically by name and
+ * the following info for each contact is included, if available:
+ *
+ * 1) contact name first, and if available, last
+ * 2) contact organization
+ * 3) phone numbers
+ * 4) email addresss
+ * 5) physical address
+ * 6) notes
+ *
+ * Running this script creates a file called contact-list.txt on the desktop that
+ * lists each contact and contact info. This script only reads the contact info and
+ * does not modify it in any way.  However, care should be taken when
+ * running any script. Please understand the code before running the script.
+ *
+ * @author jsscribe.com
+ * @licnese MIT
+ */
+
+"use strict";
+
+// create reference to Contacts application
+let contactsApp = Application("Contacts");
+
+// create reference to Script Editor application
+let scriptEditorApp = Application.currentApplication();
+scriptEditorApp.includeStandardAdditions = true;
+
+// create reference to file "contact-list.txt" on the desktop
+let desktopLocation = scriptEditorApp.pathTo("desktop").toString();
+let desktopFile = `${desktopLocation}/contact-list.txt`;
+
+// create a regex variable for use later in removing unnecesary characters "_$!<" and ">!$_" from a string later
+let regexp = /_\$!<|>!\$_/g;
+
+// get object specifier for ALL the people in contacts
+let objSpecPeople = contactsApp.people;
+
+// length property works to show number of items in array of specifiers
+// as well as in normal JavaScript arrays
+let numOfContacts = objSpecPeople.length;
+
+// get JS array of people names (first + last) from the ArraySpecifier
+// by invoking the name property on the specifier
+let jsArrayOfNames = objSpecPeople.name();
+
+
+// sort JS array ignoring upper or lower case
+jsArrayOfNames.sort(function(a, b) {
+	try{
+		let nameA = a.toUpperCase(); 
+		let nameB = b.toUpperCase(); 
+		if (nameA < nameB) {
+			return -1;
+		}
+		if (nameA > nameB) {
+			return 1;
+		}
+		// names are equal
+		return 0;
+	}catch(err){
+		console.log(err);
+	}
+});
+
+// create a string containing the info for a contact from an array index
+function createContactString(contactNumber){
+	try{
+		// check that a name exists at this index (should always be true)
+		if (jsArrayOfNames[contactNumber]){
+			
+			// get an object specifier for a person with a name value at this index
+			let objSpecPerson = objSpecPeople.byName(jsArrayOfNames[contactNumber]);
+			
+			let strInfo = `${objSpecPerson.name()}  (${contactNumber + 1})` + "\n";
+			
+			// organization info only appears if it exists
+			if (objSpecPerson.organization()) {strInfo += "Company:\n\t" + objSpecPerson.organization() + "\n";}
+
+			let aPhones = objSpecPerson.phones();
+			strInfo += "Phones:\n";
+			aPhones.forEach(function(sPhone){
+				strInfo += "\t" + `${sPhone.label()}: ${sPhone.value()}` + "\n";
+			});
+
+			let aEmails = objSpecPerson.emails();
+			strInfo += "Emails:\n"; 
+			aEmails.forEach(function(sEmailAddress){
+				strInfo += "\t" + `${sEmailAddress.label()}: ${sEmailAddress.value()}` + "\n" ;
+			});
+
+			let aAddresses = objSpecPerson.addresses();
+			strInfo += "Addresses:\n";
+			aAddresses.forEach(function(sLocation){//forEach WILL NOT work on arraySpecs, only Arrays!
+				strInfo += "\t" + `${sLocation.label()}: ${sLocation.street()}, ${sLocation.city()}, ${sLocation.state()}, ${sLocation.zip()}` + "\n";
+			});
+
+			// note info only appears if it exists
+			if (objSpecPerson.note()) {strInfo += "Notes:\n\t" + `${objSpecPerson.note()}`;}
+
+			// separator between each contact
+			strInfo += "\n\n-----------------------------------------------------\n\n\n";
+
+			// remove characters that don't render properly in the contact string.
+			// replaceAll() function effectively removes these characters "_$!<"
+			strInfo = strInfo.replaceAll(regexp, "");
+
+			// return the newly created string
+			return strInfo;
+        } else {
+			// return an error string (this should never happen)
+			let strError = "\n\n Info not available for " + `(${contactNumber + 1})` + "\n\n-------------\n\n";
+			return strError;
+		}
+	}catch(err){
+		console.log(err);
+	}
+}
+
+// open a file I/O reference for writing to a file
+function openFileAccess(filename){
+	try{
+		let openedFileRef = scriptEditorApp.openForAccess(Path(filename), { writePermission: true });
+	    return openedFileRef;
+	}catch(err){
+		let ans = scriptEditorApp.displayDialog(
+		"There is an error in trying to create the file on the desktop.\n" +
+		"Make sure that you gave the script permission to run earlier.\n" + 
+		"You may have to start over and copy/paste to a new script.\n" +
+		"Click OK to exit the Script Editor and try it again."
+		 );
+		if(ans.buttonReturned === "OK"){
+		// quit the Script Editor (which should close all open file refferences)
+		scriptEditorApp.quit(); 
+		}
+	}
+}
+// write contact info for all contacts one after another to desktop file "contact-list.txt"
+function writeToFile(file){
+    try{ // all lets under here are scoped to here only!
+
+		// open I/O access to a desktop file
+		let openedFile = openFileAccess(file);
+
+		// loop over JS Array of Contacts, create a string, and write it to the desktop file
+		// jsArrayOfNames.slice(0,3).forEach(function(el){
+		// for(let i = 0;i<numOfContacts;i++){
+        for(let i = 0; i<2; i++){	
+			// get string for contact
+			let str = createContactString(el);
+
+			// get the current EOF location
+			let locEOF = scriptEditorApp.getEof(openedFile);
+
+			// write to the file at the EOF		
+			scriptEditorApp.write(str, { to: openedFile, startingAt: locEOF });
+		} // end of for loop
+
+	// close the file I/O reference after all contact strings have been writen to the file
+	scriptEditorApp.closeAccess(openedFile);
+
+	}catch(err){
+		try {
+			// try to close the file
+			scriptEditorApp.closeAccess(openedFile);
+		}catch{
+			console.log(err);
+		}
+	}
+// this message should appear on the Script Editor console pane
+// below the editing pane
+return "script has been run";
+} 
+
+// execution of script begins here
+writeToFile(desktopFile);
+
+
